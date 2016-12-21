2185eda9c4ca5bb59ac925a62dddf7f590b83433
Evnica <evnica@gmail.com>
2016-12-21 19:26:12 +0100

    add analysis functionality and utils(division in samples for error testing)

--- interpolation/analysis.py
+++ interpolation/analysis.py
@@ -1,3 +1,11 @@
+# Class Analysis contains data on current data set: minimum and maximum of latitude / longitude / altitude  and
+# analyzed value, as well as analysis parameters (analyzed phenomenon, time step and spatial density of the
+# interpolated grid).
+
+from decimal import *
+import numpy
+
+
 class Analysis:
     def __init__(self, time_step, density):
         self.time_step = time_step
@@ -15,3 +23,41 @@ class Analysis:
         self.coos = 'WGS84'
         self.coos_unit = 'Decimal Degree (lat, lon), Meter (alt)'
         self.coords_order = '[lat, lon, alt]'
+
+    def set_phenomenon(self, phenomenon):
+        if phenomenon == 0:
+            self.phenomenon = 'Air Temperature'
+            self.phenomenon_unit = "Degree Celsius"
+        elif phenomenon == 1:
+            self.phenomenon = 'Pressure'
+            self.phenomenon_unit = "mmHg"
+        elif phenomenon == 2:
+            self.phenomenon = 'Humidity'
+            self.phenomenon_unit = "%"
+        else:
+            self.phenomenon = 'Unknown'
+            self.phenomenon_unit = "Unknown"
+
+    def set_lat_limits(self, lat_max, lat_min):
+        self.lat_max = lat_max
+        self.lat_min = lat_min
+
+    def set_lon_limits(self, lon_max, lon_min):
+        self.lon_max = lon_max
+        self.lon_min = lon_min
+
+    def set_alt_limits(self, alt_max, alt_min):
+        self.alt_max = alt_max
+        self.alt_min = alt_min
+
+    def generate_grid(self):
+        step = 1 / self.density
+        getcontext().prec = 8
+        grid = []
+        for i in range(self.density):
+            for j in range(self.density):
+                for k in range(self.density):
+                    grid.append([k * step + step / 2, j * step + step / 2, i * step + step / 2])
+        return grid
+
+    
---
+++ interpolation/utils.py
@@ -0,0 +1,40 @@
+import numpy
+
+
+# divides given points, values and timestamps into groups, each of which is composed by means of simple random sampling
+# without replacement
+def divide_in_random(num_of_random_samples, points, values, timestamps):
+    assert len(points) == len(values) == len(timestamps), 'number elements in points [%d], ' \
+           'values [%d] and timestamps [%d] must be equal' % (len(points), len(values), len(timestamps))
+    n = len(points)
+    sample_size = n // num_of_random_samples
+    rest = n - (sample_size * num_of_random_samples)
+    if rest > 0.5 * sample_size:  # if rest is more than 50% of sample size, values should be split differently
+        if rest == sample_size:
+            sample_size = (n - 1) // (num_of_random_samples - 1)
+        else:
+            sample_size = n // (num_of_random_samples - 1)
+    random_samples = numpy.empty(shape=(num_of_random_samples - 1, sample_size, 3), dtype=object)
+    #  print(random_samples)
+    sample_index = 0
+    number_of_removed_entries = 0
+    element_index = 0
+    for i in range(sample_size * (num_of_random_samples - 1)):
+        random_index = numpy.random.randint(0, n - number_of_removed_entries)
+        random_samples[sample_index][element_index] = \
+            [[points[random_index][0], points[random_index][1], points[random_index][2]],
+             values[random_index], timestamps[random_index]]
+        del (points[random_index])
+        del (values[random_index])
+        del (timestamps[random_index])
+        number_of_removed_entries += 1
+        element_index += 1
+        if element_index >= sample_size:
+            sample_index += 1
+            element_index = 0
+        last_sample = numpy.empty(shape=(len(points), 3), dtype=object)
+    for i in range(len(points)):
+        last_sample[i][0] = points[i]
+        last_sample[i][1] = values[i]
+        last_sample[i][2] = timestamps[i]
+    return random_samples, last_sample
