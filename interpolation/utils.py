81227a7906364a8b586d9129a551687e97af8359
Evnica <evnica@gmail.com>
2016-12-29 00:05:10 +0100

    fix a bug in divide in random: last_sample was created multiple times

--- interpolation/utils.py
+++ interpolation/utils.py
@@ -19,8 +19,10 @@ def divide_in_random(num_of_random_samples, points, values, timestamps):
     sample_index = 0
     number_of_removed_entries = 0
     element_index = 0
+    # print('Sample 1')
     for i in range(sample_size * (num_of_random_samples - 1)):
         random_index = numpy.random.randint(0, n - number_of_removed_entries)
+        # print(random_index)
         random_samples[sample_index][element_index] = \
             [[points[random_index][0], points[random_index][1], points[random_index][2]],
              values[random_index], timestamps[random_index]]
@@ -31,8 +33,9 @@ def divide_in_random(num_of_random_samples, points, values, timestamps):
         element_index += 1
         if element_index >= sample_size:
             sample_index += 1
+            # print("Sample " + str(sample_index+1))
             element_index = 0
-        last_sample = numpy.empty(shape=(len(points), 3), dtype=object)
+    last_sample = numpy.empty(shape=(len(points), 3), dtype=object)
     for i in range(len(points)):
         last_sample[i][0] = points[i]
         last_sample[i][1] = values[i]
